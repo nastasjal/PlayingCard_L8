43c67b28551911fa21272b3d31c574941c495840
nastasjal <nastasjal@list.ru>
2018-12-03 23:57:21 +0300

    create model, lecture 5

---
+++ PlayingCard/PlayingCard​.swift
@@ -0,0 +1,51 @@
+//
+//  PlayingCard​.swift
+//  PlayingCard
+//
+//  Created by Анастасия Латыш on 03/12/2018.
+//  Copyright © 2018 Анастасия Латыш. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCard {
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit {
+        case spades
+        case hearts
+        case clubs
+        case diamonds
+        
+        static var all = [Suit.clubs, .diamonds, .hearts, .spades]
+    }
+    
+    enum Rank {
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks = [Rank.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"), .face("Q"), .face("K")]
+            
+            return allRanks
+        }
+    }
+}
