e6c076aa434d116d2f93211f71ad0bf8c6215ae7
nastasjal <nastasjal@list.ru>
2018-12-12 17:50:22 +0300

    initial example

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -11,14 +11,47 @@ import UIKit
 class ViewController: UIViewController {
 var deck = PlayingCardDeck()
     
+  /*  @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+    */
+    
+    @IBOutlet var cardViews: [PlayingCardView]!
+    
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left , .right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print ("\(card)")
-            }
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count + 1)/2) {
+            let card = deck.draw()!
+            cards += [card, card]
         }
-        // Do any additional setup after loading the view, typically from a nib.
+        for cardView in cardViews {
+            cardView.isFaceUp = true
+            let card = cards.remove(at: 0)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+        }
+        
     }
 
 
